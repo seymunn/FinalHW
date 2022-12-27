bc15fa3bdd72b180ebac53537583bd1fc4648ed1
seymunn <april.jons.xd@gmail.com>
2022-12-27 13:47:49 +0300

    add second part of Task

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,15 @@
 ﻿string[] firstarray = new string[4] {"hello", "2", "world", ":-)"};
 string[] resularray = new string[firstarray.Length];
+
+void GetResultArray(string[] array1, string[] array2)
+{
+    int num = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[num] = array1[i];
+            num += 1;
+        }
+    }
+}
